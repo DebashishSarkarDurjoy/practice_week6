e6eadb4bed2500529e11e90b08ee46d40db6d581
DebashishSarkarDurjoy <dsdjoy4@gmail.com>
2023-02-10 16:43:31 +1100

    max sub array practice 2

--- stacksNQueue/maxSubArray.cpp
+++ stacksNQueue/maxSubArray.cpp
@@ -4,9 +4,10 @@
 
 using namespace std;
 
-void maxSubArray(vector<int> &numbers, int k) {
-    deque<int> d(k);
+vector<int> maxSubArray(vector<int> &numbers, int k) {
+    vector<int> result;
 
+    deque<int> d(k);
     int i = 0;
     for (; i < 3; i++) {
         while ( (!d.empty()) && (numbers[d.back()] < numbers[i]) ) {
@@ -15,21 +16,27 @@ void maxSubArray(vector<int> &numbers, int k) {
         d.push_back(i);
     }
 
-    for (; i < numbers.size(); i++) {
-        cout << d.front() << " ";
+    for (; i <= numbers.size(); i++) {
+        result.push_back( numbers[d.front()] );
         while ( (!d.empty()) && (d.front() <= i-k) ) {
             d.pop_front();
         }
         while ( (!d.empty()) && (numbers[d.back()] < numbers[i]) ) {
             d.pop_back();
         }
-        d.push_back(i);   
+        d.push_back(i);
     }
+
+    return result;
 }
 
 int main(void) {
-    vector<int> numbers = {1,2,3,1,4,5,2,3,5};
-    maxSubArray(numbers, 3);
+    vector<int> numbers = {1,3,-1,-3,5,3,6,7};
+    
+    
+    for (int n: maxSubArray(numbers, 3)) {
+        cout << n << endl;
+    }
 
     return 0;
 }
